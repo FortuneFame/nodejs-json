0581ec0ed07552f162e3e3c876d7e25de7c6ff29
Marchenko Yaroslava <fortunefame1710@gmail.com>
2023-09-28 22:21:43 +0200

    Enhanced server setup

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path')
+
 const apiRouter = require('./api/apiRoutes')
 
 const app = express();
 const PORT = 3000;
 
 apiRouter(app)
+
+app.use(express.static('./public'));
+
+app.all('*', (req, res) => {
+    res.status(404).sendFile(path.resolve(__dirname, './public/404.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running: http://localhost:${PORT}/`)
